358840842fc3a3192e4a8f6f1946f70d1fcc8250
John Pape <jpapejr@icloud.com>
2021-01-25 13:52:02 -0500

    Update provider.tf

--- szr-roks-on-classic/provider.tf
+++ szr-roks-on-classic/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.20.0"
+    }
+  }
+}
+
 provider "ibm" {
   iaas_classic_username = var.sl_user
   iaas_classic_api_key = var.sl_api_key
