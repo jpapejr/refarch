f364192419e5a417d577632b457ae18b0ca8bcb0
John Pape <jpapejr@icloud.com>
2021-01-25 10:52:01 -0500

    Update provider.tf

--- szr-roks-on-vpc/provider.tf
+++ szr-roks-on-vpc/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.12.0"
+    }
+  }
+}
+
 provider "ibm" {
   generation = 2
   region = var.region
