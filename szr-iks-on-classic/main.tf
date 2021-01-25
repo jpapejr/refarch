40f859a39f9419402ab38c13612a4d0a3223b71f
John Pape <jpapejr@icloud.com>
2021-01-25 14:58:03 -0500

    Update main.tf

--- szr-iks-on-classic/main.tf
+++ szr-iks-on-classic/main.tf
@@ -30,14 +30,16 @@ data "ibm_resource_group" "resource_group" {
 }
 
 resource "ibm_container_cluster" "cluster" {
-  name                 = "${var.cluster_name}-${random_id.name1.hex}"
-  datacenter           = var.zone
-  default_pool_size    = var.worker_count
-  machine_type         = var.cluster_node_flavor
-  hardware             = "shared"
-  kube_version         = var.cluster_kube_version
-  public_vlan_id       = ibm_network_vlan.public_vlan.id
-  private_vlan_id      = ibm_network_vlan.private_vlan.id
-  force_delete_storage = true
+  name                   = "${var.cluster_name}-${random_id.name1.hex}"
+  datacenter             = var.zone
+  default_pool_size      = var.worker_count
+  machine_type           = var.cluster_node_flavor
+  hardware               = "shared"
+  kube_version           = var.cluster_kube_version
+  public_vlan_id         = ibm_network_vlan.public_vlan.id
+  private_vlan_id        = ibm_network_vlan.private_vlan.id
+  force_delete_storage   = true
+  wait_for_worker_update = false
+  update_all_workers     = false
 
 }
