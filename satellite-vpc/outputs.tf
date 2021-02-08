6541672f563e3727bd3ced7073d0a43491f46877
jpapejr <jpapejr@icloud.com>
2021-02-08 13:32:46 -0500

    index the nic array

--- satellite-vpc/outputs.tf
+++ satellite-vpc/outputs.tf
@@ -3,25 +3,25 @@ output "vpc" {
 }
 
 output "cp-1" {
-  value = ibm_is_instance.sat-host-cp-1.primary_network_interface.primary_ipv4_address
+  value = ibm_is_instance.sat-host-cp-1.primary_network_interface.primary_ipv4_address[0]
 }
 
 output "cp-2" {
-  value = ibm_is_instance.sat-host-cp-2.primary_network_interface.primary_ipv4_address
+  value = ibm_is_instance.sat-host-cp-2.primary_network_interface.primary_ipv4_address[0]
 }
 
 output "cp-3" {
-  value = ibm_is_instance.sat-host-cp-3.primary_network_interface.primary_ipv4_address
+  value = ibm_is_instance.sat-host-cp-3.primary_network_interface.primary_ipv4_address[0]
 }
 
 output "cl-1" {
-  value = ibm_is_instance.sat-host-cl-1.primary_network_interface.primary_ipv4_address
+  value = ibm_is_instance.sat-host-cl-1.primary_network_interface.primary_ipv4_address[0]
 }
 
 output "cl-2" {
-  value = ibm_is_instance.sat-host-cl-2.primary_network_interface.primary_ipv4_address
+  value = ibm_is_instance.sat-host-cl-2.primary_network_interface.primary_ipv4_address[0]
 }
 
 output "cl-3" {
-  value = ibm_is_instance.sat-host-cl-3.primary_network_interface.primary_ipv4_address
+  value = ibm_is_instance.sat-host-cl-3.primary_network_interface.primary_ipv4_address[0]
 }
